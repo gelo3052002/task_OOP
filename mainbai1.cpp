1bd14bdeb838502fb44ca7a0474a9021ed7c9cb3
Nguyễn Quốc Khánh <80594865+BruceSteven@users.noreply.github.com>
2021-04-14 19:52:31 +0700

    Update mainbai1.cpp

--- mainbai1.cpp
+++ mainbai1.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
+#define MAX 100
+struct Team {
+    string name;
+    string soluongthanhvien;
+
+}
 class contest 
 {
     private:
@@ -8,31 +14,56 @@ class contest
         string Nhatochuc;
         string Thoigian;
         string Hinhthucthi;
-
         //Jeopardy
         //Attack & Defence;
         //Kết hợp
-        string Nhatochuc;
-        string Danhsachteam;
+        Team Danhsachteam[MAX];
     public:
         contest();
         ~contest();
-        void Dangki();
-        void CheckTeamExsit();
+        void input() {
+            cout << "Ten cuoc thi: ";
+            cin >> Tencuocthi;
+            cout << "Nha to chuc: ";
+            cin >> Nhatochuc;
+            cout <<"Thoi gian: ";
+            cin >> Thoigian;
+            cout << "Hinh thuc thi(Jeopardy, Attack & Defence , Kết hợp):" << endl;
+            cin >> Hinhthucthi;
+        }
+        void Dangki() {
+            cout << "Nhap ten team: ";
+            cin >> name;
+            cout << "So luong thanh vien: ";
+            cin >> soluongthanhvien;
+        };
+        void CheckTeamExsit() {
+            
+        };
         void List3TeamBestScore();
 }
 class CTF 
 {   
     private:
-        contest Cuocthi[100];
+        contest Cuocthi[MAX];
         int quantity;
     public:
         CTF();
         ~CTF();
-
+    void input() {
+        cout << "So cuoc thi CTF muon to chuc: ";
+        cin >> quantity;
+        for(int i = 0;i < n;i++) {
+            Cuocthi[i].input();
+        }
+    }
 
 };
 CTF::CTF(){
 }
 CTF::~CTF(){
+}
+int main() {
+    CTF uit;
+    uit.input();
 }
