b4b0459d088ddeb333ae99572de41848cb084780
Nguyễn Quốc Khánh <80594865+BruceSteven@users.noreply.github.com>
2021-04-14 15:23:41 +0700

    Update mainbai1.cpp

--- mainbai1.cpp
+++ mainbai1.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
-#include "fraction.h"
+#include <string>
 using namespace std;
- 
-int main()
+class contest 
 {
-    PhanSo a, b, c;
-    a.NhapPhanSo();b.NhapPhanSo();
-    cout << "Cong : ";
-    c = a;c.Cong(b);c.XuatPhanSo();
-    cout << "Tru : ";
-    c = a;c.Tru(b);c.XuatPhanSo();
-    cout << "Nhan : ";
-    c = a;c.Nhan(b);c.XuatPhanSo();
-    cout << "Chia : ";
-    c = a;c.Chia(b);c.XuatPhanSo();
-    return 0;
+    private:
+        string Tencuocthi;
+        string Nhatochuc;
+        string Thoigian;
+        string Hinhthucthi;
+
+        //Jeopardy
+        //Attack & Defence;
+        //Kết hợp
+        string Nhatochuc;
+        string Danhsachteam;
+    public:
+        contest();
+        ~contest();
+        void Dangki();
+        void CheckTeamExsit();
+        void List3TeamBestScore();
+}
+class CTF 
+{   
+    private:
+        contest Cuocthi[100];
+        int quantity;
+    public:
+        CTF();
+        ~CTF();
+
+
+};
+CTF::CTF(){
+}
+CTF::~CTF(){
 }
