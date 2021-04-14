41bfc01111aa617fc9754023119a8e3c3221a0e6
haoln-sys <82515666+haoln-sys@users.noreply.github.com>
2021-04-14 20:21:40 +0700

    Update and rename main2 to main2.cpp

---
+++ main2.cpp
@@ -0,0 +1,88 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class cuocthi
+{
+private:
+	char ten[50];
+	char nhatochuc[50];
+	char thoigian[50];
+	char hinhthuc[50];
+	doithi danhsach;
+public:
+	cuocthi();
+	~cuocthi();
+	void nhap();
+	void xuat();
+	void cuocthithamgianhieunhat();
+};
+class doithi
+{
+private:
+	char tendoi[60];
+	int soluong;
+public:
+	doithi();
+	~doithi();
+	void nhap();
+	void check();
+};
+class CTF
+{
+private:
+	int n;
+	cuocthi* quanly = new cuocthi[n];
+public:
+	CTF();
+	~CTF();
+	void nhap();
+};
+cuocthi::cuocthi() {}
+cuocthi::~cuocthi() {}
+doithi::doithi() {}
+doithi::~doithi() {}
+CTF::CTF() {}
+CTF::~CTF()
+{
+	delete quanly;
+}
+void CTF::nhap()
+{
+	cout << "nhap so luong cuoc thi: ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		quanly[i].nhap();
+	}
+}
+void cuocthi::nhap()
+{
+	cout << "ten cuoc thi: ";
+	cin >> ten;
+	cout << " nha to chuc: ";
+	cin.ignore();
+	cin >> nhatochuc;
+	cout << "thoi gian: ";
+	cin.ignore();
+	cin >> thoigian;
+	cout << "hinh thuc thi : ";
+	cin.ignore();
+	cout << hinhthuc;
+	cout << "doi tham gia: ";
+}
+void cuocthi::xuat()
+{
+	cout << ten << "\t" << nhatochuc << "\t" << hinhthuc << "\t" << thoigian;
+}
+void cuocthi::cuocthithamgianhieunhat() {}
+void doithi::nhap()
+{
+	cout << "nhap ten doi: ";
+	cin >> tendoi;
+	cout << "nhap so luong thanh vien: ";
+	cin.ignore();
+	cin >> soluong;
+}
+void doithi::check() {}
+
